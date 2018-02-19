6601778f21176ccf2f34c4e618fb23e32ad88c07
Jordan Jones <jordanalanjones@gmail.com>
2018-02-18 22:27:45 -0600

    Updating comments, and adding insert functionality to userController

--- src/main/java/com/houndshobbies/registration/controller/UserController.java
+++ src/main/java/com/houndshobbies/registration/controller/UserController.java
@@ -2,8 +2,10 @@ package com.houndshobbies.registration.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 import com.houndshobbies.registration.Greeting;
 import org.apache.log4j.Logger;
 import org.apache.log4j.LogManager;
@@ -22,29 +24,42 @@ public class UserController {
 		dao = new GenDao(User.class);
 	}
 
-	@RequestMapping("/users")
-	public List<User> greeting(@RequestParam(value="firstName", defaultValue="null") String name) {
-		logger.info("Test!");
+	/**
+	 * This function handles getting all users
+	 * @return users a list of all users
+	 */
+	@RequestMapping(value = "/users", method = RequestMethod.GET)
+	public List<User> getAll() {
 		List<User> users = dao.getAll();
 		return users;
 	}
 
-	@RequestMapping("/insert")
-	public int insert() {
-		User user = new User("Eric", "Jones", "ejones@wisc.edu", 1, "t");
+	/**
+	 * This function handles the insert of a user
+	 * @param user the User object that is sent in with the request body
+	 */
+	@RequestMapping(value = "/insert", method = RequestMethod.POST)
+	public int insert(@RequestBody User user) {
 		return dao.insert(user);
 	}
 
-	@RequestMapping("/update")
-	public String update() {
-		User user = dao.getById(9);
-		user.setFirstName("New");
+	/**
+	 * This function handles the update of a user
+	 * @param user the User object that is sent in with the request body
+	 */
+	@RequestMapping(value = "/update", method = RequestMethod.POST)
+	public String update(@RequestBody User user) {
 		dao.saveOrUpdate(user);
 		return "Hopefully Updated!";
 	}
 
-	public String delete() {
-		User user = dao.getById(9);
+	/**
+	 * This function handles a delete of the user
+	 * @param user the User object that is sent in with the request body
+	 */
+	@RequestMapping(value = "/delete", method = RequestMethod.POST)
+	public User delete(@RequestBody User user) {
 		dao.delete(user);
+		return user;
 	}
 }
