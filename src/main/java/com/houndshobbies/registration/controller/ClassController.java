529bcb322e828356a619599352a9e634ab6ed589
Jordan Jones <jordanalanjones@gmail.com>
2018-02-19 19:02:18 -0600

    Adding class controller

--- src/main/java/com/houndshobbies/registration/controller/ClassController.java
+++ src/main/java/com/houndshobbies/registration/controller/ClassController.java
@@ -3,6 +3,8 @@ package com.houndshobbies.registration.controller;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.apache.log4j.Logger;
 import org.apache.log4j.LogManager;
@@ -21,9 +23,40 @@ public class ClassController {
 		dao = new GenDao(Class.class);
 	}
 
+	/**
+	 * This function returns all of the classes.
+	 * @return classes
+	 */
 	@RequestMapping("/classes")
 	public List<Class> classes() {
 		List<Class> classes = dao.getAll();
 		return classes;
+	}
+
+	/**
+	 * This funciton inserts a new class into the database.
+	 * @param eventClass the class sent by the client to insert
+	 */
+	@RequestMapping(value = "/classes/insert", method = RequestMethod.POST)
+	public int insert(@RequestBody Class eventClass) {
+		return dao.insert(eventClass);
+	}
+
+	/**
+	 * This function updates an class.
+	 * @param eventClass the class sent by the client to update
+	 */
+	@RequestMapping(value = "/classes/update", method = RequestMethod.POST)
+	public void update(@RequestBody Class eventClass) {
+		dao.saveOrUpdate(eventClass);
+	}
+
+	/**
+	 * This function deletes an event from the table.
+	 * @param eventClass the Class object sent in by the client
+	 */
+	@RequestMapping(value = "/classes/delete", method = RequestMethod.POST)
+	public void delete(@RequestBody Class eventClass) {
+		dao.delete(eventClass);
 	}
 }
