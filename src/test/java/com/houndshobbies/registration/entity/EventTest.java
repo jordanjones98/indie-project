25dc7b7f463b4d1e4af8f778a49ccb2e98ad7761
Jordan Jones <jordanalanjones@gmail.com>
2018-02-20 14:51:27 -0600

    Adding tests for Events, and Users

---
+++ src/test/java/com/houndshobbies/registration/entity/EventTest.java
@@ -0,0 +1,36 @@
+package com.houndshobbies.registration.entity;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.houndshobbies.registration.persistence.GenDao;
+import com.houndshobbies.registration.entity.Event;
+import com.houndshobbies.registration.entity.Class;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.runner.RunWith;
+import java.util.List;
+
+@SpringBootTest
+public class EventTest {
+
+	GenDao dao;
+	GenDao classDao;
+
+	@BeforeEach
+	void setUp() {
+		dao = new GenDao(Event.class);
+		classDao = new GenDao(Event.class);
+	}
+
+	@Test
+	void addClassToEventSuccess() {
+		Event event = (Event)dao.getById(2);
+		Class eventClass = (Class)classDao.getById(1);
+		event.addClass(eventClass);
+		dao.saveOrUpdate(event);
+		Event newEvent = (Event)dao.getById(2);
+		assertEquals(1, newEvent.getClasses().size());
+	}
+}
---
+++ src/test/java/com/houndshobbies/registration/entity/UserTest.java
@@ -0,0 +1,24 @@
+package com.houndshobbies.registration.entity;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.houndshobbies.registration.persistence.GenDao;
+import com.houndshobbies.registration.entity.User;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.runner.RunWith;
+import java.util.List;
+
+@SpringBootTest
+public class UserTest {
+
+	GenDao dao;
+
+	@BeforeEach
+	void setUp() {
+		dao = new GenDao(Event.class);
+	}
+
+}
